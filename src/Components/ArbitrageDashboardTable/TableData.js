ce873ea15e660c22ed51f5fdad484dacd427c957
Jeff Oliver <jeffoliver3384@gmail.com>
2019-12-19 18:57:33 -0500

    added exchange color styles to arbitrage dashboard

--- src/Components/ArbitrageDashboardTable/TableData.js
+++ src/Components/ArbitrageDashboardTable/TableData.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
 
-const TableData = ({ item }) => {
-  return <td>{item}</td>;
+const useStyles = makeStyles({
+  binanceColor: {
+    color: "rgb(78, 185, 255)"
+  },
+  coinbaseColor: {
+    color: "rgb(83, 207, 215)"
+  },
+  bitfinex: {
+    color: "rgb(98, 227, 171)"
+  }
+});
+
+const TableData = ({ item, positive, negative }) => {
+  const classes = useStyles();
+
+  if (positive && item >= 0) {
+    return <td className={positive}>{item}</td>;
+  } else if (negative && negative) {
+    return <td className={negative}>{item}</td>;
+  } else if (item === "Binance") {
+    return <td className={classes.binanceColor}>{item}</td>;
+  } else if (item === "Coinbase_pro") {
+    return <td className={classes.coinbaseColor}>{item}</td>;
+  } else if (item === "Bitfinex") {
+    return <td className={classes.bitfinex}>{item}</td>;
+  } else {
+    return <td>{item}</td>;
+  }
 };
 
 export default TableData;
--- src/Components/ArbitrageDashboardTable/TableDataRow.js
+++ src/Components/ArbitrageDashboardTable/TableDataRow.js
@@ -9,6 +9,12 @@ const useStyles = makeStyles({
     background: "rgba(0, 0, 0, 0.8)",
     color: "rgb(255, 255, 255)",
     borderBottom: "0.5px solid rgba(35, 32, 44, 0.8)"
+  },
+  positive: {
+    color: "green"
+  },
+  negative: {
+    color: "red"
   }
 });
 
@@ -27,9 +33,19 @@ const TableDataRow = ({ dataRow }) => {
   const data = Object.values(dataRow);
   return (
     <TableRow className={classes.tablerow}>
-      {data.map(item => (
-        <TableData item={item} />
-      ))}
+      {data.map((item, i) => {
+        if (i === 6) {
+          return (
+            <TableData
+              item={item}
+              positive={classes.positive}
+              negative={classes.negative}
+            />
+          );
+        } else {
+          return <TableData item={item} />;
+        }
+      })}
     </TableRow>
   );
 };
