0f95e4c5bf5cc9e4d07ac636343b9d07910ad40b
masonkarsevar <masonkarsevar@gmail.com>
2019-12-18 16:55:57 -0800

    fixed navigation branch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import NavigationHeader from "./Components/NavigationHeader";
+import NavigationBar from "./Components/NavigationBar";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <NavigationHeader />
+      <NavigationBar />
+    </div>
+  );
 }
 
 export default App;
