272fd5a42037d7a4e30626d143380f1c3d29157f
Luis Mendes <cvlopes88@gmail.com>
2020-01-06 12:38:18 -0500

    footer changes

---
+++ src/Components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Footer = () => {
+
+    return (
+        <div>
+            
+        <nav className='footer'>
+            <a>Cryptolytic © 2020</a>
+            <a>Privacy & Legal</a>
+            <a>Contact</a>
+            <a>About us</a>
+        </nav>
+        </div>
+    )
+
+}
+
+export default Footer;
