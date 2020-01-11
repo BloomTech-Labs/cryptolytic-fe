e37a3dd5166f6a1bd550588b9dad039476b81dd8
micah riley <micahriley88@gmail.com>
2020-01-11 03:43:37 -0500

    Created reusable DashboardTable component based off the existing Arbitrage and Trading DashboardTable components. New component expects prop data.

---
+++ src/Components/DashboardTable.js
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { css } from "@emotion/core";
+import PropagateLoader from "react-spinners/PropagateLoader";
+import { makeStyles } from "@material-ui/core/styles";
+import TableContainer from "@material-ui/core/TableContainer";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+
+import TableHeaderRow from "./TradingDashboardTable/TableHeaderRow"
+import TableHeaderRow from "./TradingDashboardTable/TableDataRow"
+
+import TableHeaderRow from "./ArbitrageDashboardTable/TableHeaderRow"
+import TableDataRow from "./ArbitrageDashboardTable/TableDataRow"
+
+const useStyles = makeStyles({
+  table: {
+    marginLeft: "20vw",
+    width: "70%",
+    background: "rgba(35, 32, 44, 0.8)",
+    boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+    "&::-webkit-scrollbar-thumb": {
+      backgroundColor: "rgba(255,255,255,1.0)",
+      outline: "1px solid slategrey"
+    }
+  },
+  tableContainer: {
+    maxHeight: 800,
+
+    "&::-webkit-scrollbar": {
+      display: "none"
+    }
+  }
+});
+
+const override = css`
+  display: flex;
+  justify-content: center;
+  position: relative;
+  margin: 30vh 0 50vh 10vw;
+`;
+
+const TradingDashboardTable = (props) => {
+  const classes = useStyles();
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get(`${props.endpoint}`)
+      .then(res => {
+        setData(res.data);
+        setTimeout(() => {
+          setLoading(false);
+        }, 2000);
+      })
+      .catch(error => {
+        console.log("error", error);
+      });
+  }, []);
+
+  const tableData = data.map(obj => {
+    let {props.dataColumns} = obj;
+
+    let date, time;
+    trade_time = new Date(obj.trade_time * 1000);
+
+    date = trade_time.toLocaleDateString();
+    time = trade_time.toLocaleTimeString();
+
+    return props.tableColumns;
+  });
+
+  const headerData = [];
+
+  for (let key in tradingData[0]) {
+    headerData.push(key);
+  }
+
+  if (loading) {
+    return (
+      <PropagateLoader
+        css={override}
+        size={25}
+        color={
+          "linear-gradient(93.16deg, #4EB9FF 19.25%, #53CFD7 45.13%, #5DDCB7 67.95%, #62E3AB 82.93%);"
+        }
+        loading={loading}
+      />
+    );
+  } else {
+    return (
+      <TableContainer className={classes.tableContainer}>
+        <Table stickyHeader aria-label='sticky table' className={classes.table}>
+          <TableHead>
+            <props.TableHeaderRow headerData={props.headerData} />
+          </TableHead>
+          <TableBody>
+            {props.rowData.map(e => (
+              <TableDataRow dataRow={e} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  }
+};
+
+export default TradingDashboardTable;
