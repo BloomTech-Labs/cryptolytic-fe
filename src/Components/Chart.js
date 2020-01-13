1862961ca562500a481f633961a64a8d3ae75b1a
Jeff Oliver <jeffoliver3384@gmail.com>
2020-01-13 14:08:41 -0500

    created component for canvasjs chart

---
+++ src/Components/Chart.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Chart extends Component {
+  render() {
+    return (
+      <>
+        <h1>Chart</h1>
+      </>
+    );
+  }
+}
+
+export default Chart;
