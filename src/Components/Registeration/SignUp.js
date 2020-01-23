72e6957b803538749cba4c27d449d5b641edb7be
Luis Mendes <cvlopes88@gmail.com>
2020-01-23 14:46:30 -0500

    match sign up design to login

--- src/Components/Registeration/SignUp.js
+++ src/Components/Registeration/SignUp.js
@@ -1,36 +1,56 @@
 import React, { useCallback } from "react";
 import { withRouter } from "react-router";
 import app from "./base";
-import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from "@material-ui/core/styles";
 
+
+
+const useStyles = makeStyles(theme => ({
+  form: {
+    width: "25%",
+    margin: "0 auto",
+    height: "40vh"
+  },
+  formContainer: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    padding: 5,
+  },
+  button: {
+    width: "30%",
+    margin: "0 auto",
+    transition: ".4s ease",
+    "&:hover": {
+      backgroundColor: "#161616",
+      color: "#dddddd"
+    }
+  },
+  textField: {
+    width: "75%",
+  },
+  cssOutlinedInput: {
+    '&$cssFocused $notchedOutline': {
+      borderColor: `gray !important`,
+    }
+  },
+  cssFocused:{
+    color: "black !important"
+  },
+  notchedOutline: {
+    borderWidth: '1px',
+    borderColor: 'black !important',
+  },
+}));
 
 const SignUp = ({ history }) => {
 
-
-
-
-  const useStyles = makeStyles(theme => ({
-    
-    root: {
-      '& > *': {
-        margin: theme.spacing(1),
-        width: 200,
-        backgroundColor: "#4EB9FF",
-      },
-      color: "#4EB9FF",
-
-    },
-  }));
-  
-  
-  
   const classes = useStyles();
-  
 
-
-  
   const handleSignUp = useCallback(async event => {
     event.preventDefault();
     const { email, password } = event.target.elements;
@@ -45,19 +65,46 @@ const SignUp = ({ history }) => {
   }, [history]);
 
   return (
-    <div className="signupForm">
+   <div>
       <h1>Sign up</h1>
-      <form className={classes.root} onSubmit={handleSignUp}>
-       <div className="emailField">
-          <TextField name="email" label="Email" type="email"  variant="filled"  />
-        </div>
-           <div className="passField">
-          <TextField name="password" label="Password" type="password"  variant="filled" />
-        </div>
-      
-        <Button variant="contained" color="blue" type="submit">Sign Up</Button>
+      <Paper className={classes.form}>
+      <form className={classes.formContainer} onSubmit={handleSignUp}>
+      <div className="emailField">
+          <TextField name="email" label="Email" type="email" size="small"  variant="outlined"
+          className={classes.textField} 
+          InputProps={{
+            classes: {
+              root: classes.cssOutlinedInput,
+              focused: classes.cssFocused,
+              notchedOutline: classes.notchedOutline,
+            },
+          }}
+          InputLabelProps={{
+            classes: {
+              focused: classes.cssFocused,
+            },
+          }}
+          />
+          </div>
+          <div className="passField">
+          <TextField name="password" label="Password" type="password"  variant="filled" className={classes.textField}
+            InputProps={{
+              classes: {
+                root: classes.cssOutlinedInput,
+                focused: classes.cssFocused,
+                notchedOutline: classes.notchedOutline,
+              },
+            }}
+            InputLabelProps={{
+              classes: {
+                focused: classes.cssFocused,
+              },
+            }}/>
         
+          </div>
+        <Button variant="contained" type="submit" className={classes.button} >Sign Up</Button>
       </form>
+    </Paper>
     </div>
   );
 };
