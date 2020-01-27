3e83bbc6868f8bab7227e31387e122c75f7c673c
micah riley <micahriley88@gmail.com>
2020-01-27 14:38:42 -0500

    Refactored inline styling into makeStyle classes located inside each component. Added hover effects for clickable elements.

--- src/Components/Marketing Page/Header.js
+++ src/Components/Marketing Page/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
 
 import Link from '@material-ui/core/Link';
 
@@ -9,23 +10,99 @@ const data = [
     {name: "Team", href: "/intro/team"}
 ]
 
+const useStyles = makeStyles({
+    headerContainer: {
+        width: '100%', 
+        height: '8vh', 
+        backgroundColor: 'black', 
+        display: 'flex', 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        color: 'white', 
+        fontFamily: 'Titillium Web', 
+        fontSize: '22px', 
+        lineHeight: '40px'
+    },
+    headerLeft: {
+        width: '50%', 
+        display: 'flex', 
+        justifyContent: 'flex-start'
+    },
+    headerLogoContainer: {
+        display: 'flex', 
+        justifyContent: 'flex-start', 
+        alignItems: 'center', 
+        width: '50%', 
+        fontSize: '40px', 
+        lineHeight: '76px', 
+        marginLeft: '1em'
+    },
+    headerLogoLink: {
+        textDecoration: 'none', 
+        display: 'flex', 
+        alignItems: 'center', 
+        color: 'white',
+        '&:hover': {
+            textDecoration: 'none'
+        }
+    },
+    headerLogo: {
+        width: '1.2em'
+    },
+    headerRightContainer: {
+        width: '70%', 
+        display: 'flex', 
+        justifyContent: 'flex-end' 
+    },
+    headerMenuContainer: {
+        display: 'flex', 
+        justifyContent: 'space-around', 
+        width: '50%',
+    },
+    headerMenuLink: {
+        textDecoration: 'none', 
+        display: 'flex', 
+        alignItems: 'center', 
+        color: 'white',
+        '&:hover': {
+            textDecoration: 'none',
+            color: '#4EB9FF'
+        }
+    },
+    headerMenuLogin: {
+        backgroundColor: '#4EB9FF', 
+        borderRadius: '4px', 
+        padding: '2px 40px', 
+        textDecoration: 'none', 
+        display: 'flex', 
+        alignItems: 'center', 
+        color: 'white',
+        '&:hover': {
+            background: 'white',
+            color: '#4EB9FF',
+            textDecoration: 'none'
+        }
+    }
+  });
+
 export default function Header(){
+    const classes = useStyles();
     return(
-        <div style={{ width: '100%', height: '8vh', backgroundColor: 'black', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'white', fontFamily: 'Titillium Web', fontSize: '22px', lineHeight: '40px' }}>
-            <div style={{ width: '50%', display: 'flex', justifyContent: 'flex-start'}}>
-                <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', width: '50%', fontSize: '40px', lineHeight: '76px', marginLeft: '1em' }}>
-                    <Link href="/intro" style={{ textDecoration: 'none', display: 'flex', alignItems: 'center', color: 'white' }}>
-                        <img src={logo} style={{ width: '1.2em' }}/>
+        <div className={classes.headerContainer}>
+            <div className={classes.headerLeft}>
+                <div className={classes.headerLogoContainer}>
+                    <Link href="/intro" className={classes.headerLogoLink}>
+                        <img src={logo} className={classes.headerLogo}/>
                         <p>Cryptolytic</p>
                     </Link>
                 </div>
             </div>
-            <div style={{ width: '70%', display: 'flex', justifyContent: 'flex-end' }}>
-                <div style={{ display: 'flex', justifyContent: 'space-around', width: '50%' }}>
+            <div className={classes.headerRightContainer}>
+                <div className={classes.headerMenuContainer}>
                     {data.map(e => 
-                        <Link href={e.href} style={{ textDecoration: 'none', display: 'flex', alignItems: 'center', color: 'white' }}>{e.name}</Link>
+                        <Link href={e.href} className={classes.headerMenuLink}>{e.name}</Link>
                         )}
-                    <Link href="/home" style={{ backgroundColor: '#4EB9FF', borderRadius: '4px', padding: '2px 40px', textDecoration: 'none', display: 'flex', alignItems: 'center', color: 'white' }}>Login</Link>
+                    <Link href="/home" className={classes.headerMenuLogin}>Login</Link>
                 </div>
             </div>
         </div>
--- src/Components/Marketing Page/HomeFeatures.js
+++ src/Components/Marketing Page/HomeFeatures.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { makeStyles } from '@material-ui/core/styles';
+
 import background from "../../../src/images/Vector 1.png"
 
 import featureOne from "../../../src/images/artboard_1 (1) 2.png"
@@ -24,15 +26,86 @@ const data = [
     reverse: false,
     },
 ]
+
+    const useStyles = makeStyles({
+        homeFeatureContainer: {
+            width: '100%', 
+            height: '300vh', 
+            backgroundImage: `url("${background}")`, 
+            backgroundRepeat: 'no-repeat', 
+            backgroundPosition: 'center top', 
+            backgroundSize: 'cover', 
+            marginTop: '-12vh', 
+            color: 'white', 
+            fontFamily: 'Titillium Web', 
+            fontWeight: 'bold', 
+            fontSize: '18px', 
+            lineHeight: '29px', 
+            textShadow: '0px 4px 10px rgba(255, 255, 255, 0.1)'
+        },
+        homeFeatureRow: {
+            width: '100%', 
+            height: '50vh', 
+            display: 'flex', 
+            alignItems: 'center', 
+            padding: '8vh 0' 
+        },
+        homeFeatureRowImage: {
+            width: '60%', 
+            display: 'flex'
+        },
+        homeFeatureRowText: {
+            width: '60%', 
+            textAlign: 'left', 
+            display: 'flex',
+        },
+        homeFeatureRowTitle: {
+            fontSize: '45px', 
+            lineHeight: '45px', 
+            letterSpacing: '0.03em'
+        },
+        homeFeatureLastRow: {
+            width: '100%', 
+            height: '50vh', 
+            display: 'flex', 
+            justifyContent: 'flex-end',
+            alignItems: 'center', 
+            padding: '5vh 0'
+        },
+        homeFeatureLastRowText: {
+            width: '30%', 
+            textAlign: 'left', 
+            display: 'flex', 
+            justifyContent: 'flex-end', 
+            marginLeft: '10vw'
+        },
+        homeFeatureLastRowTitle:
+        {
+            fontSize: '45px', 
+            lineHeight: '45px', 
+            letterSpacing: '0.03em', 
+            position: 'absolute'
+        },
+        homeFeatureLastRowSubtitle: {
+            marginTop: '6em'
+        },
+        homeFeatureLastRowImage: {
+            width: '70%', 
+            display: 'flex', 
+            justifyContent: 'flex-start'
+        }
+  });
 export default function HomeFeatures(){
+    const classes = useStyles();
     let reverse = {
     row: "",
     leftJustify: "",
     rightJustify: "",
     textAlign: ""
-}
+    }
+
     return(
-        <div style={{ width: '100%', height: '300vh', backgroundImage: `url("${background}")`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center top', backgroundSize: 'cover', marginTop: '-12vh', color: 'white', fontFamily: 'Titillium Web', fontWeight: 'bold', fontSize: '18px', lineHeight: '29px', textShadow: '0px 4px 10px rgba(255, 255, 255, 0.1)' }}>
+        <div className={classes.homeFeatureContainer}>
             <div style={{ paddingTop: '30vh' }}>
                 {data.map(e => {
                 if(e.reverse) {
@@ -47,13 +120,13 @@ export default function HomeFeatures(){
                     reverse.textAlign = 'left'
                     }
                     return (
-                    <div style={{ width: '100%', height: '50vh', display: 'flex', alignItems: 'center', flexDirection: reverse.row, padding: '8vh 0' }}>
-                        <div style={{ width: '60%', display: 'flex', justifyContent: reverse.leftJustify }}>
+                    <div className={classes.homeFeatureRow} style={{ flexDirection: reverse.row }}>
+                        <div className={classes.homeFeatureRowImage} style={{ justifyContent: reverse.leftJustify }}>
                             <img src={e.image} />
                         </div>
-                        <div style={{ width: '60%', textAlign: 'left', display: 'flex', justifyContent: reverse.rightJustify, textAlign: reverse.textAlign }}>
+                        <div className={classes.homeFeatureRowText} style={{ justifyContent: reverse.rightJustify, textAlign: reverse.textAlign }}>
                             <div>
-                                <h2 style={{ fontSize: '45px', lineHeight: '45px', letterSpacing: '0.03em' }}>{e.title}</h2>
+                                <h2 className={classes.homeFeatureRowTitle}>{e.title}</h2>
                                 <p>{e.subTitle}</p>
                             </div>
                         </div>
@@ -62,14 +135,14 @@ export default function HomeFeatures(){
                 }
             )}
             </div>
-            <div style={{ width: '100%', height: '50vh', display: 'flex', justifyContent: 'flex-end', alignItems: 'center', padding: '5vh 0' }}>
-                <div style={{ width: '30%', textAlign: 'left', display: 'flex', justifyContent: 'flex-end', marginLeft: '10vw' }}>
+            <div className={classes.homeFeatureLastRow}>
+                <div className={classes.homeFeatureLastRowText}>
                     <div>
-                        <h2 style={{ fontSize: '45px', lineHeight: '45px', letterSpacing: '0.03em', position: 'absolute' }}>Automate your trading process</h2>
-                        <p style={{ marginTop: '6em' }}>The cryptocurrency market doesn’t sleep and neither should your money! Connect the Cryptolytic API to a trading bot and act on trading and arbitrage opportunities automatically 24/7!</p>
+                        <h2 className={classes.homeFeatureLastRowTitle}>Automate your trading process</h2>
+                        <p className={classes.homeFeatureLastRowSubtitle}>The cryptocurrency market doesn’t sleep and neither should your money! Connect the Cryptolytic API to a trading bot and act on trading and arbitrage opportunities automatically 24/7!</p>
                     </div>
                 </div>
-                <div style={{ width: '70%', display: 'flex', justifyContent: 'flex-start'}}>
+                <div className={classes.homeFeatureLastRowImage}>
                     <img src={featureFour} />
                 </div>
             </div>
